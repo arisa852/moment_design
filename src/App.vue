cbcab5bbfa87ae25ca44e704c2268adb30c98448
arisa852 <arisa852@gmail.com>
2024-06-05 21:39:49 +0800

    feat: homepage

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,97 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  
+import logo from '@/assets/images/mdlogo.svg';
+//import { App } from 'ant-design-vue';
+
+export default {
+  data() {
+    return {
+      imageUrl: logo
+}  
+  }
+}
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+ <header>
+  
+ <div class="pageheader">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+ <a-page-header
+      class="demo-page-header"
+      style="display: flex;justify-content: space-between;" 
+      title="Moment-Design"
+      sub-title="專屬你的時刻，由你來設計"
+     
+    >
+   
+    <a-input-search
+      v-model:value="value"
+      placeholder="input search text"
+      style="width: 300px;margin-right: 300px;"
+      @search="onSearch"
+    />
+  
+     
+    <a-button key="1" style="margin-right: 20px;font-size: 16px;"
+    >登入</a-button>
+   <a-button key="2"
+        style="margin-right: 20px;font-size: 16px;">EN</a-button>
+   <a-button key="3"
+        style="margin-right: 20px;font-size: 16px;">白</a-button>
+        
+ </a-page-header>
+ 
+</div>
+ </header>
 
-  <RouterView />
+ <router-view></router-view>
+ <footer></footer>
+
+ 
+
+
+
+
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  background-color:#A9D4F1;
+  color: #fff;
+ 
+.pageheader{
+
+  max-width: 1280px;
+  margin:0 auto;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+ 
+ }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
 
-nav a:first-of-type {
-  border: 0;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+
+
+
 </style>
+
+
+
+
+
+
+
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,177 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import Banner from '@/assets/images/banner-1.png'
+import Container from '@/assets/images/pic1.png'
+import { ref } from 'vue';
+export default {
+  name: 'Banner',
+  name:'container'
+  
+
+
+}
+const loading = ref(true);
+
+const handleClick = () => {
+  loading.value = !loading.value;
+};
+
 </script>
+
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="banner">
+    <div class="text-block">
+      <h1>設計你的時刻</h1>
+      <p>從今天起，化繁為簡</p>
+    </div>
+    <div class="image-block">
+      <img src="@/assets/images/banner-1.png" alt="Banner">
+    </div>
+  </div>
+  <div class="cards">
+  <div style="padding: 20px;width: 100%;max-width: 1280px;margin:0 auto;" >
+    <a-row :gutter="16">
+      <a-col :span="8">
+        <a-card>
+          <p style="font-size: 20px;line-height: 1.5em;font-weight: bold;text-align: center;">任何時刻的解決方案</p>
+          <p style="font-size: 16px;line-height: 1.5em;text-align: center;" >開啟一個充滿可能性的世界，再也不會感到陷入困境。讓你的每一步都越走越踏實</p>
+        </a-card>
+      </a-col>
+      <a-col :span="8">
+        <a-card >
+          <p style="font-size: 16px;line-height: 1.5em;font-weight: bold;text-align: center;">逐步指南</p>
+          <p style="font-size: 16px;line-height: 1.5em;text-align: center;">每個操作指南都提供了易於遵循的逐步說明，可幫助您自信地解決問題。</p>
+        </a-card>
+      </a-col>
+      <a-col :span="8">
+        <a-card>
+          <p style="font-size: 16px;line-height: 1.5em;font-weight: bold;text-align: center;">啟發並受到啟發</p>
+          <p style="font-size: 16px;line-height: 1.5em;text-align: center;">加入我們充滿活力的學習者和創造者社群。創造一個知識共享和賦權的社群。</p>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
+</div>
+<div class="container">
+    <div class="image-container">
+      <img src="@/assets/images/pic1.png" alt="Image" />
+    </div>
+    <div class="loading-card">
+      <div class="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  </div>
+   
+<div class="card-container">
+  <div class="image-container">
+      <img src="@/assets/images/pic1.png" alt="Image" />
+    </div>
+    <div style="background: #ececec; padding: 30px">
+    <a-card title="Card title" :bordered="false" style="width: 300px">
+      <p>Card content</p>
+      
+    </a-card>
+  </div>
+  <div style="background: #ececec; padding: 30px">
+    <a-card title="Card title" :bordered="false" style="width: 300px">
+      <p>Card content</p>
+    
+    </a-card>
+  </div>
+
+
+</div>
+
   </main>
 </template>
+
+<style scoped lang="scss">
+
+
+h1{
+  font-size: 40px;
+  line-height: 1.5em;
+}
+p{
+  font-size: 28px;
+}
+.banner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #E2E6E8;
+  width: 100%;
+  overflow: hidden;
+ 
+
+  .text-block, .image-block {
+    flex: 1;
+    margin-top: 20px;
+    margin-left: 150px;
+    margin-right: 150px;
+    padding:20px;
+    
+  }
+
+  .image-block img {
+    width: 100%;
+   
+   
+  }
+ .cards{
+  background-color: #E2E6E8;
+
+ }
+
+ .container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+
+.image-container {
+ 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+
+.image-container img{
+  max-width: 100%;
+  width: 100px;
+}
+
+.loading-card {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ccc;
+  padding: 20px;
+  border-radius: 10px;
+ 
+}
+
+.spinner {
+  width: 40px;
+  height: 40px;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  
+}
+.card-container{
+  display: flex;
+}
+
+
+
+
+}
+
+
+</style>
